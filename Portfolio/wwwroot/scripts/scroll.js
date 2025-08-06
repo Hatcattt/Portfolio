b2b9f7a077ebbb2d753b1a9b40cd4edb63b341d6
hatcattt <hatcattt.dev@gmail.com>
2025-08-06 13:46:24 +0200

    add a scroll up to top button

---
+++ Portfolio/wwwroot/scripts/scroll.js
@@ -0,0 +1,15 @@
+﻿window.initScrollTopButton = () => {
+    const btn = document.getElementById("scrollTopBtn");
+
+    window.addEventListener("scroll", () => {
+        if (document.documentElement.scrollTop > 100) {
+            btn.style.display = "block";
+        } else {
+            btn.style.display = "none";
+        }
+    });
+};
+
+window.scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
